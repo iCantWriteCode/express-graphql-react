67af0fee2317c22f5f139bd9f07c0bd3d6f1145b
Andreas <andrchas@gmail.com>
2019-04-17 23:01:11 +0300

    Add, Delete mutetions

--- schema/schema.js
+++ schema/schema.js
@@ -2,7 +2,7 @@ const graphql = require('graphql');
 // const _ = require('lodash');
 const axios = require('axios');
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLNonNull } = graphql;
 
 const CompanyType = new GraphQLObjectType({
 	name: 'Company',
@@ -54,6 +54,35 @@ const RootQuery = new GraphQLObjectType({
 	}
 });
 
+const mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addUser: {
+			type: UserType,
+			args: {
+				firstName: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) },
+				company: { type: GraphQLString }
+			},
+			resolve(parentValue, { firstName, age }) {
+				return axios.post('http://localhost:3000/users', { firstName, age }).then((res) => res.data);
+			}
+		},
+		deleteUser: {
+			type: UserType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) }
+			},
+			resolve(parentValue, { id }) {
+				return axios.delete(`http://localhost:3000/users/${id}`).then((res) => {
+					res.data;
+				});
+			}
+		}
+	}
+});
+
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation
 });
