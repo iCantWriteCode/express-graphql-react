a5d634e41f8deb4c1395c2f49ef1a17c4fe35b8a
Andreas <andrchas@gmail.com>
2019-04-17 21:25:37 +0300

    express-graphql init with static data

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+
+const schema = require('./schema/schema');
+
+const port = 4000;
+
+const app = express();
+
+app.use(
+	'/graphql',
+	expressGraphQL({
+		schema, // schema = schema
+		graphiql: true
+	})
+);
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
